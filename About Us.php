76ead9c8bf1e32102ca726653f117c5e0c669b6a
SN963 <72389122+SN963@users.noreply.github.com>
2021-05-09 22:58:23 +0200

    Upgrades in UserSide

--- About Us.php
+++ About Us.php
@@ -2,136 +2,45 @@
     
     <head>
         <meta charset="utf-8">
-        <title>team-section</title>
-    <style>
-*{
-    margin:0;
-        padding:0;
-        font-family:sans:serif;
-        
-        }        
-        
-        
-.team-section
-        {
-        overflow:hidden;
-        text-align:center;
-        padding:60px
-            
-        }
+        <link rel="shortcut icon"  href="Images/LB_Editedb.png">
+        <link rel="stylesheet" type="text/css" href="About_us.css">
+        <title>About Afnan</title>
     
-.team-section h1
-        {
-        text-transform:uppercase;
-        margin-bottom:60px;
-        color:black;
-        font-size:40px;    
-        }        
-    
-.border
-        {
-            display: block;
-            margin: auto;
-            width: 160px;
-            height: 3px;
-            margin-bottom: 40px;
-                
-        }
-    
-        .ps
-        {
-            margin-bottom: 40px;
-            
-            
-        }
-        
-        
-        .ps a
-        {
-            display: inline-block;
-            margin: 0 40px;
-            width: 250px;
-            height: 500px;
-            overflow: hidden;
-            border-radius: 0%;
-            
-        }
-        
-   .ps a img
-        {
-            width: 100%;
-            border-radius: 0px;
-            filter: grayscale(100%);
-            transition: 0.4s all;
-        }
- .ps a:hover >img
-        {
-           filter:none;
-            
-            
-        }
-        
-        .section{
-            width: 600px;
-            margin: auto;
-            font-size: 20px;
-            color: white;
-            text-align: justify;
-            height: 0px;
-            overflow: hidden;
-        }
-        
-        .section:target{
-            height: auto;
-            
-        }
-        
-        .name{
-            display: block;
-            margin-bottom: 30px;
-            text-transform: uppercase;
-            font-size: 22px;
-            
-        }
-        
-        </style>
-        
-        
     </head>
     
     <body>
-       <?php include "The_New_Nav.html"?>
+       <?php include "Main_Nav.php"?>
         
         <div class="team-section">
-            <br> <br>
+            <br><h2>Our Photography</h2><br>
+            <p>Click on the photo to Know more about Afnan</p>
             <span class="border"></span>
         <div class="ps">
-            <a href="#p1"><img src="icon.jpg" alt="Afnan"></a>  
+            <a href="#p1"><img src="images/icon.jpg" alt="Afnan"></a>  
         </div>
          <div class = "section" id = "p1">
             <span class ="name"> Afnan Mohamed Mahmoud Gharid </span>
              <span class ="border"> </span>
-             <p style="color: black;">
+             <p>
               <h2>The Greatest places that I worked in:</h2>
               <br>
-                 <h4>.police Mosque<br>
-                 .The Castle<br>
-                 .Tobio Maadi<br>
-                 .Al Azhar Park<br>
-                 .Camp Ismailia<br>
-                 .January 25 Ismailia<br></h4>
-              <br>
+                 <h4>.Police Mosque<br><br>
+                 .The Castle<br><br>
+                 .Tobio Maadi<br><br>
+                 .Al Azhar Park<br><br>
+                 .Camp Ismailia<br><br>
+                 .January 25 Ismailia</h4>
               <h2>Years of experience : 2 years</h2>
+              <br><br>
               <h2>The Greatest places that I worked in:</h2>
               <br>
                  <h4>
-                 .Benha<br>
-                 .Ash Sharqia<br>
-                 .Ismailia<br>
-                 .Cairo<br>
-                 .Port Said<br>
+                 .Benha<br><br>
+                 .Ash Sharqia<br><br>
+                 .Ismailia<br><br>
+                 .Cairo<br><br>
+                 .Port Said<br><br>
                   </h4>
-              <br>
              </p>
             </div>    
         </div>       
