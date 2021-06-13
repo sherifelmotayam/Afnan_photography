487ec31d95c20ed64d3d399534daef28607ea90c
sherif elmotayam <sherifelmotayam@gmail.com>
2021-06-14 01:31:32 +0200

    edit in book and reserve

--- reserve.php
+++ reserve.php
@@ -1,7 +1,9 @@
 <?php
-  include 'Admin.php';
-require_once('database_Connect.php');
-  ?>
+    declare(strict_types=1 );
+    include 'Admin.php';
+    include 'includes/class-autoloaded.php';
+
+?>
   <head>
     <link rel="stylesheet" type="text/css" href="reserve.css">
   </head>
@@ -17,48 +19,7 @@ require_once('database_Connect.php');
           <th style = 'text-align: center; font-size:20px; border:1px solid black;'> Select </th>
       </tr>
     <?php
-    class reserve extends dbConnect
-    {
-      public function viewprofile()
-      {  
-        $sql="SELECT * FROM booking";
-        $stmt=$this->connect()->query($sql);
-        while($row=$stmt->fetch())
-        {
-    
-       print "<tr>";
-       print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['ID']."</td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['FirstName']." ".$row['LastName']."</td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['Time']."</td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['City']."</td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'>".$row['Comment']."</td>";
-          print"<td style = 'text-align: center; font-size:20px; border:1px solid black;'><input type='checkbox' name='delete[]' value='<?= .$id. ?>'></td>";
-          print "<td style = 'text-align: center; font-size:20px; border:1px solid black;'><input type='checkbox' id='checkItem' name='checkbox[]' value='<? =.$id.; ?>' ></td>";
-          print "</tr>";
-          
-          /*echo "<tr>";
-          echo "<td>".$row['ID']."</td>";
-          echo "<td><input type='checkbox' name=check[] value='".$row['ID']."'</td>";
-          echo "<tr>";*/
-
-        }
-        echo "<div class='sub'><input type= 'submit' name='delete' id='delete' value='Delete Records'>";
-        if(isset($_POST['delete']))
-          {
-            $checkbox = $_POST['check'];
-            foreach($checkbox as $id)
-            {
-              //$del_id = $checkbox[$i]; 
-               $dele="DELETE FROM `booking` where ID =".$id;
-                $r=$this->connect()->query($dele);
-                $message = "Data deleted successfully !";
-                echo $message;
-                echo $dele;
-            }
-      }
-
-      }
-    }
+  
     ?>
     <?php
     $get=new reserve();
